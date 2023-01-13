2646d92bd38ad68c37bf4ff94cf1cb3a4bde979a
EnzoXD2 <96305131+EnzoXD2@users.noreply.github.com>
2023-01-13 12:45:08 -0300

    bagui dos creditos lol

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1161,8 +1161,13 @@ class PlayState extends MusicBeatState
 		botplayTxt.borderSize = 1.25;
 		botplayTxt.visible = cpuControlled;
 		add(botplayTxt);
+	
+	        var creditTxt = new FlxText(876, 648, 348);
+    creditTxt.text = "Port By Enzo and FNF BR"; creditTxt.setFormat(Paths.font("vcr.ttf"), 30, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
+    creditTxt.scrollFactor.set();
+    add(creditTxt);
 		if(ClientPrefs.downScroll) {
-			botplayTxt.y = timeBarBG.y - 78;
+			 creditTxt.y = 148;
 		}
 
 		strumLineNotes.cameras = [camHUD];
