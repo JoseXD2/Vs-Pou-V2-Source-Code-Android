d66db1327a359e6383de6a0d0f654c1b2de07a2d
EnzoXD2 <96305131+EnzoXD2@users.noreply.github.com>
2023-01-13 18:36:55 -0300

    Update tilin.lua

--- assets/preload/custom_events/tilin.lua
+++ assets/preload/custom_events/tilin.lua
@@ -10,7 +10,7 @@ function onCreate()
 end
 
 function onEvent(name, value1, value2)
-    if name == "tilin" then
+    if name == "tilin" and mechanics then
     --Get Dodge time
     DodgeTime = (value1);
 	
